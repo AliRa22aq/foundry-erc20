e9a06d67f3d6239c4fbc5af41a3791ba97c1896c
Ali Razzaq <alirazzaquet@gmail.com>
2025-05-06 12:46:55 -0400

    Project added

---
+++ src/OurToken.sol
@@ -0,0 +1,13 @@
+// contracts/OurToken.sol
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.24;
+
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+// import {ERC20} from "../lib/openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
+
+contract OurToken is ERC20 {
+    constructor(uint256 initialSupply) ERC20("OurToken", "OT") {
+        _mint(msg.sender, initialSupply);
+    }
+}
